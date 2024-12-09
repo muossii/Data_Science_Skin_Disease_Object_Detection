4766494f04997d75eed6d23b6fbbcd80ed941873
mosta <mostafakhafaji2004@gmail.com>
2024-12-09 21:21:24 +0800

    object detection python files. Object detection with ultralytics module preffered.

--- Object_Detection_yolo_Module.py
+++ Object_Detection_yolo_Module.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 # Load pre-trained model
 model = YOLO('best.pt')
 
-# run inferrence on the source
-results = model(source=0, show=True, conf=0.4, save=True) #generator of results objects
+# run inference on the source
+results = model(source=0, show=True, conf=0.2, save=True) #generator of results objects
+
 
